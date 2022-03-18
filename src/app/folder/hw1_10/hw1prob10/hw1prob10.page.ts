866078d1d1740c40b579471272f2c7e14b6835fe
Devin Wilson <Devin.Wilson@4rhuskies.org>
2022-03-18 09:58:18 -0500

    first commit

---
+++ src/app/folder/hw1_10/hw1prob10/hw1prob10.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hw1prob10',
+  templateUrl: './hw1prob10.page.html',
+  styleUrls: ['./hw1prob10.page.scss'],
+})
+export class Hw1prob10Page implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/folder/madlib/madlib/madlib.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-madlib',
+  templateUrl: './madlib.page.html',
+  styleUrls: ['./madlib.page.scss'],
+})
+export class MadlibPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
